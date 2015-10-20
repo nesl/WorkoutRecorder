54e2c43a37459bf9be1822fac4dcbe5972d74d59
Chenguang Shen <pscggeforce@gmail.com>
2015-10-20 15:35:14 -0700

    first version of standalone watch app

---
+++ wear/src/main/java/edu/ucla/nesl/android/workoutrecorder/BGDataCollectionService.java
@@ -0,0 +1,123 @@
+package edu.ucla.nesl.android.workoutrecorder;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BGDataCollectionService extends Service implements SensorEventListener {
+    private static final String TAG = "BGService";
+    private final IBinder mBinder = new MyBinder();
+
+    // Make everything static so that each bind will get the same SensorManager and PrintWriter
+    private static SensorManager mSensorManager;
+    private static ArrayList<Sensor> sensors = new ArrayList<>();
+    private static Map<Integer, PrintWriter> sensorType2Logger = new HashMap<>();
+
+    private static PrintWriter loggerAcc;
+    private static PrintWriter loggerGyro;
+    private static PrintWriter loggerMag;
+    private static PrintWriter loggerGrav;
+
+    public BGDataCollectionService() {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        // for recording the time offset
+        int sensorType = sensorEvent.sensor.getType();
+        long now = System.currentTimeMillis();
+
+        String line = now + "," + sensorEvent.timestamp;
+        for (float v : sensorEvent.values)
+            line += "," + v;
+        line += "\n";  // for JSON stringify
+
+        try {
+            sensorType2Logger.get(sensorType).write(line);
+            sensorType2Logger.get(sensorType).flush();
+        } catch (Exception e) {
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public class MyBinder extends Binder {
+        public BGDataCollectionService getService() {
+            return BGDataCollectionService.this;
+        }
+    }
+
+    public void startRecording(String timestring) {
+        Log.i(TAG, "start recording");
+
+        mSensorManager = ((SensorManager) getSystemService(SENSOR_SERVICE));
+        sensors.add(mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+        sensors.add(mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
+        sensors.add(mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD));
+        sensors.add(mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY));
+
+        String folder_prefix = "/sdcard/wear_"
+                + timestring + "_";
+
+        try {
+            loggerAcc = new PrintWriter(folder_prefix + "acc.csv");
+            loggerGyro = new PrintWriter(folder_prefix + "gyro.csv");
+            loggerMag = new PrintWriter(folder_prefix + "mag.csv");
+            loggerGrav = new PrintWriter(folder_prefix + "grav.csv");
+        } catch (Exception e) {
+        }
+
+        sensorType2Logger.put(Sensor.TYPE_ACCELEROMETER,  loggerAcc);
+        sensorType2Logger.put(Sensor.TYPE_GYROSCOPE,      loggerGyro);
+        sensorType2Logger.put(Sensor.TYPE_MAGNETIC_FIELD, loggerMag);
+        sensorType2Logger.put(Sensor.TYPE_GRAVITY, loggerGrav);
+
+        // register sensors
+        registerAllSensors();
+    }
+
+    public void stopRecording() {
+        Log.i(TAG, "stop recording");
+
+        unregisterAllSensors();
+        for (int sensor_id : sensorType2Logger.keySet()) {
+            try {
+                sensorType2Logger.get(sensor_id).flush();
+                sensorType2Logger.get(sensor_id).close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private void registerAllSensors() {
+        for (Sensor sensor: sensors) {
+            mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+    }
+
+    private void unregisterAllSensors() {
+        for (Sensor sensor: sensors) {
+            mSensorManager.unregisterListener(this, sensor);
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i(TAG, "onBind");
+        return mBinder;
+    }
+}
---
+++ wear/src/main/java/edu/ucla/nesl/android/workoutrecorder/MainActivity.java
@@ -0,0 +1,112 @@
+package edu.ucla.nesl.android.workoutrecorder;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView mTextView;
+    private BGDataCollectionService serviceInstance = null;
+    private static final String TAG = "Activity";
+    // Set format for date and time
+    private static TimeString mTimestring = new TimeString();
+    // Current state of the recording
+    private boolean mTracking = false;
+    private String mTime = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                // Get saved recording state from shared preference
+                SharedPreferences sharedPref = getSharedPreferences(
+                        getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                mTracking = sharedPref.getBoolean(getString(R.string.saved_rec_flag), false);
+                if (mTracking) {
+                    mTime = sharedPref.getString(getString(R.string.saved_rec_time), "");
+                    mTextView.setText("Tracking started at " + mTime);
+                }
+                else {
+                    mTextView.setText("Tracking stopped");
+                }
+            }
+        });
+    }
+
+    public void onStartClicked(View view) {
+        Log.i(TAG, "start clicked");
+        // Start recording
+        if (serviceInstance != null) {
+            mTracking = true;
+            mTime = mTimestring.currentTimeForDisplay();
+            serviceInstance.startRecording(mTimestring.currentTimeForFile());
+            mTextView.setText("Tracking started at " + mTime);
+        }
+    }
+
+    public void onStopClicked(View view) {
+        Log.i(TAG, "stop clicked");
+        // Stop recording
+        if (serviceInstance != null) {
+            serviceInstance.stopRecording();
+            mTextView.setText("Tracking stopped");
+            mTracking = false;
+            mTime = null;
+        }
+    }
+
+    protected void onPause() {
+        super.onPause();
+        // Unbind to service on pause
+        unbindService(mConnection);
+        serviceInstance = null;
+        // Save current recording state to shared preference
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.saved_rec_flag), mTracking);
+        if (mTracking) {
+            editor.putString(getString(R.string.saved_rec_time), mTime);
+        }
+        else {
+            editor.putString(getString(R.string.saved_rec_time), null);
+        }
+        editor.commit();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        // Bind to service on resume
+        Intent intent= new Intent(this, BGDataCollectionService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder b) {
+            BGDataCollectionService.MyBinder binder = (BGDataCollectionService.MyBinder) b;
+            serviceInstance = binder.getService();
+            Log.i(TAG, "service connected");
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            serviceInstance = null;
+            Log.i(TAG, "service DISconnected");
+        }
+    };
+}
